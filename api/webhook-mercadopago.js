b8aec3ba6652e0235cdbc0710b5e1b4a20030555
CLENA TV <tvclena@gmail.com>
2026-01-18 16:18:29 -0300

    Update webhook-mercadopago.js

--- api/webhook-mercadopago.js
+++ api/webhook-mercadopago.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Método não permitido" });
   }
 
-  
+
   try {
     const body =
       typeof req.body === "string"
