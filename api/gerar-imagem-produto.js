a5a9276e9181a840b54025dd14a1714afb077a32
CLENA TV <tvclena@gmail.com>
2026-01-21 02:46:25 -0300

    Update gerar-imagem-produto.js

--- api/gerar-imagem-produto.js
+++ api/gerar-imagem-produto.js
@@ -8,7 +8,7 @@ const openai = new OpenAI({
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ROLE
 );
 
 export default async function handler(req, res) {
